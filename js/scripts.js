e43ccbf46bc2114674efe2c47591c78af7263c50
jashua212 <jashua212@gmail.com>
2018-05-13 12:11:17 -0400

    formatting: covert space to tabs

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 let trainedNet = train(trainingData);
 
 function encode(arg) {
-    return arg.split('').map(function (x) {
+	return arg.split('').map(function (x) {
 		return x.charCodeAt(0) / 256;
 	});
 }
@@ -10,43 +10,43 @@ function encode(arg) {
 // helper function
 // encodes the 'input' property of each object contained in the training data (array)
 function processTrainingData(data) {
-    return data.map(function (obj) {
-        return {
-            input: encode(obj.input),
-            output: obj.output
-        };
-    });
+	return data.map(function (obj) {
+		return {
+			input: encode(obj.input),
+			output: obj.output
+		};
+	});
 }
 
 // trains the neural network, using the provided training data (array)
 function train(data) {
-    let net = new brain.NeuralNetwork();
-    net.train(processTrainingData(data));
+	let net = new brain.NeuralNetwork();
+	net.train(processTrainingData(data));
 
 	// return the trained neural network as a function
-    return net.toFunction();
+	return net.toFunction();
 }
 
 function execute(tweet) {
-    let results = trainedNet(encode(tweet));
-    console.dir(results);
+	let results = trainedNet(encode(tweet));
+	console.dir(results);
 
-    let output;
-    let certainty;
+	let output;
+	let certainty;
 
-    if (results.trump > results.kardashian) {
-        output = 'Donald Trump';
-        certainty = Math.floor(results.trump * 100);
+	if (results.trump > results.kardashian) {
+		output = 'Donald Trump';
+		certainty = Math.floor(results.trump * 100);
 
 	} else {
-        output = 'Kim Kardashian';
-        certainty = Math.floor(results.kardashian * 100);
-    }
+		output = 'Kim Kardashian';
+		certainty = Math.floor(results.kardashian * 100);
+	}
 
 	console.log(typeof certainty, certainty);
 	certainty = certainty ? certainty + '%' : 'not';
 
-    return "I'm " + certainty + " sure that the tweet was written by " + output;
+	return "I'm " + certainty + " sure that the tweet was written by " + output;
 }
 
 let tweetInp = document.querySelector('.tweet');
